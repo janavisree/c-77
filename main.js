cb3afc96b6642f80650d6797df84acec82c29319
janavisree <94519430+janavisree@users.noreply.github.com>
2022-07-17 15:15:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var first_name = "Rabir";
+var last_name = "kapoor";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+var a = 10;
+var b = 3;
+var add = a+b;
+console.log(add);
+var sub = a-b;
+console.log(sub);
